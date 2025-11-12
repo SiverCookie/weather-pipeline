c0a72e550d4e667ea39d07c2092cd313505b0a57
SiverCookie <vladp.opris@gmail.com>
2025-11-12 20:12:05 +0200

    Update 1010333843833434343434

--- src/db.py
+++ src/db.py
@@ -1,12 +1,12 @@
 # src/db.py
+import os
 import psycopg
 
 def get_connection():
     return psycopg.connect(
-        dbname="weather_data",
-        user="weather_user",
-        password="0000",
-        host="localhost",
-        port=5432
+        dbname=os.getenv("DB_NAME", "weather_data"),
+        user=os.getenv("DB_USER", "weather_user"),
+        password=os.getenv("DB_PASS", "0000"),
+        host=os.getenv("DB_HOST", "localhost"),  # va fi "postgres" pe GitHub Actions
+        port=os.getenv("DB_PORT", "5432")
     )
-
